6f15d05a0263a9905bd8cd69580b37ec07f759d6
kkumykova <38696352+kkumykova@users.noreply.github.com>
2023-01-09 12:44:20 +0000

    Update recursion.js
    Use Recursion to Create a Range of Numbers

--- recursion.js
+++ recursion.js
@@ -70,3 +70,24 @@ function countdown(n){
   }
 }
 console.log(countdown(5));
+
+
+/*
+Use Recursion to Create a Range of Numbers
+
+The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter.
+The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind.
+It should also work for cases where both startNum and endNum are the same.
+*/
+
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const countArray = rangeOfNumbers(startNum, endNum - 1 );
+    countArray.push(endNum)
+    return countArray;  
+  }
+};
+
+console.log(rangeOfNumbers(1,5))
